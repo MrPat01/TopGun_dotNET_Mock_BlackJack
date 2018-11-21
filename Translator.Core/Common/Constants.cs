8a6cd550a241653f16234d6e2cc6538faa1ff9ff
purple-sunset <purple.sunshine.alone@gmail.com>
2018-11-21 18:43:53 +0700

    Done Read, Save Excel

---
+++ Translator.Core/Common/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translator.Core.Common
+{
+    public class Constants
+    {
+        public static readonly string FileAppendJp2Vn = "_japanese";
+    }
+
+    public enum TranslateType
+    {
+        Vn2Jp,
+        Jp2Vn
+    }
+}
---
+++ Translator.Core/Services/ExcelService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+using Translator.Core.Common;
+
+namespace Translator.Core.Services
+{
+    public class ExcelService
+    {
+        private string TranslateText(string text, TranslateType type)
+        {
+            return String.Empty;
+        }
+
+        private void Statistic(long wordCount, long pageCount)
+        {
+            return;
+        }
+
+        public void Translate(string inputPath, string outputPath, TranslateType type)
+        {
+            var fi = new FileInfo(inputPath);
+
+            using (var package = new ExcelPackage(fi))
+            {
+                var listSheet = package.Workbook.Worksheets;
+                for (int i = 1; i <= listSheet.Count(); i++)
+                {
+                    var currentSheet = listSheet[i];
+
+                    //Translate sheet name
+                    currentSheet.Name = TranslateText(currentSheet.Name, type);
+
+                    //Translate each cell
+                    for (int c = 1; c <= currentSheet.Dimension.End.Column; c++)
+                    {
+                        for (int r = 1; r <= currentSheet.Dimension.End.Row; r++)
+                        {
+                            currentSheet.Cells[r, c].Value = TranslateText(currentSheet.Cells[r, c].Value.ToString(), type);
+                        }
+                    }
+                }
+
+                var outputFile = new FileInfo(outputPath);
+                package.SaveAs(outputFile);
+            }
+        }
+    }
+}
