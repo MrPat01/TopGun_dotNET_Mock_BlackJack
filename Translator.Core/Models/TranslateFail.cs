43d8ae9a62cbd52f784106a17dbd36e7fc71c9e0
dangnh2002 <dangnh2002@gmail.com>
2018-11-27 17:13:33 +0700

    add TranslateFail

---
+++ Translator.Core/Models/TranslateFail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Translator.Core.Common;
+
+namespace Translator.Core.Models
+{
+    public class TranslateFail : BaseEntity
+    {
+        public string Text { get; set; }
+        public TranslateType typeId { get; set; }
+    }
+}
---
+++ Translator.Core/Services/TranslateFailService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Translator.Core.IServices;
+using Translator.Core.Models;
+
+namespace Translator.Core.Services
+{
+    public class TranslateFailService : Repository<TranslateFail>, ITranslateFailService
+    {
+        public TranslateFailService(TranslatorContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
