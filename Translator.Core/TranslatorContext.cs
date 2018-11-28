0af4ce0c370c2c1b33253dab0a0a0d6f0ae72540
dangnh2002 <dangnh2002@gmail.com>
2018-11-28 17:17:14 +0700

    add timeout

--- Translator.Core/TranslatorContext.cs
+++ Translator.Core/TranslatorContext.cs
@@ -9,6 +9,7 @@ namespace Translator.Core
             : base("name=TranslatorContext")
         {
             Database.SetInitializer<TranslatorContext>(new TranslatorInitializer());
+            this.Database.CommandTimeout = 600;
         }
 
         public virtual DbSet<ConfigPath> ConfigPath { get; set; }
